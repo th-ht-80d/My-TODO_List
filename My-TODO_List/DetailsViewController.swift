45fed0fe8d3027d473c725bce8df5342afc85ace
MyToDo <https://github.com/th-ht-80d/MyToDo.git>
2021-01-17 21:46:27 +0900

    first commit

---
+++ My-TODO_List/DetailsViewController.swift
@@ -0,0 +1,23 @@
+//
+//  Details.swift
+//  My-TODO_List
+//
+//  Created by 濱貴大 on 2021/01/17.
+//
+
+import UIKit
+
+
+class DetailsController: UIViewController {
+    
+    
+    
+    
+    override func viewDidLoad() {
+   
+        super.viewDidLoad()
+        
+        
+    }
+    
+}
