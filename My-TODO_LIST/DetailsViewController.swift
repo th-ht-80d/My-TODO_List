3befbbd74039da34a9f79f1a562b73f5ed627781
hama <21331104th@tama.ac.jp>
2021-01-28 21:07:12 +0900

    Main.storyboard修正

--- My-TODO_LIST/DetailsViewController.swift
+++ My-TODO_LIST/DetailsViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 
+var DetailsText = [String]()
 
 class DetailsViewController: UIViewController, UITextFieldDelegate {
     
@@ -15,19 +16,32 @@ class DetailsViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var TextDetails: UITextView!
     
     
+    @IBAction func DetailsAdd(_ sender: Any) {
+        
+        DetailsText.append(TextDetails.text!)
+        
+        //追加ボタンを押したらフィールドを空にする
+        TextDetails.text = ""
+    }
+    
+    
     override func viewDidLoad() {
         
         super.viewDidLoad()
         
         TextDetails.text = ""
         
-        
     }
+    
+    
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         TextDetails.text = textField.text
         
         textField.resignFirstResponder()
         return true
+        
+        
+        
     }
     
 }
